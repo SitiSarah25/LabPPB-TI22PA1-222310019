b483f71b41bc69a7ab55f4459b0db9adc217df4d
Siti Sarah <113155306+SitiSarah25@users.noreply.github.com>
2024-03-10 22:41:58 +0700

    Add files via upload

---
+++ Latihan Pertemuan 1/components/Mobil.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+const Mobil = () => {
+    return (
+        <View>
+        <Text>SITI SARAH</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default Mobil;
